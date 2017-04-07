37048e4a843f25e7050c9958d3c191eabd315dee
Justas <samuolis.just@gmail.com>
2017-04-07 20:30:14 +0300

    Add styles to weather page

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -11,9 +11,7 @@ var Weather = React.createClass({
   },
   handleSearch: function (location) {
     var that = this;
-
     this.setState({isLoading: true});
-
     openWeatherMap.getTemp(location).then(function (temp) {
       that.setState({
         location: location,
@@ -27,18 +25,16 @@ var Weather = React.createClass({
   },
   render: function () {
     var { isLoading, temp, location } = this.state;
-
     function renderMessage () {
       if (isLoading) {
-        return <h3>Fetching weather...</h3>;
+        return <h3 className="text-center">Fetching weather...</h3>;
       } else if (temp && location) {
         return <WeatherMessage temp={temp} location={location}/>;
       }
     }
-
     return (
       <div>
-        <h3>Weather Component</h3>
+        <h1 className="text-center">Get Weather</h1>
         <WeatherForm onSearch={this.handleSearch}/>
         {renderMessage()}
       </div>
